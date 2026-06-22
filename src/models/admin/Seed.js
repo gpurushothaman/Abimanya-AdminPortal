37889921a45f46b62338eed4fbf193771f0920e5
21ec043Thiyagu <146075318+21ec043Thiyagu@users.noreply.github.com>
2026-06-22 18:23:27 +0530

    updated seed.js

--- src/models/admin/Seed.js
+++ src/models/admin/Seed.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 
 const Dimension = require("./Dimension");
 const DoorThreshold = require("./DoorThreshold");
+const DoorOrientation = require("./DoorOrientation");
+const DoorJambLocation =require("./DoorJambLocation");
+const DoorFrame = require("./DoorFrame")
 
 async function seedDimensions() {
   const exists = await Dimension.exists({});
@@ -36,12 +39,96 @@ async function seedDoorThresholds() {
   }
 }
 
+async function seedDoorOrientations() {
+  const exists = await DoorOrientation.exists({});
+
+  if (!exists) {
+    await DoorOrientation.insertMany([
+      {
+        DoorOrientationname: "LHS",
+        DoorOrientationvalue: "lhs",
+      },
+      {
+       DoorOrientationname: "RHS",
+       DoorOrientationvalue: "rhs",
+      },
+    ]);
+
+    console.log("DoorOrientation seed completed");
+  } else {
+    console.log("DoorOrientation already seeded");
+  }
+}
+
+
+async function seedDoorjamblocations() {
+  const exists = await DoorJambLocation.exists({});
+
+  if (!exists) {
+    await DoorJambLocation.insertMany([
+      {
+       jambLocationName: "Front",
+        jambLocationValue: "front",
+      },
+      {
+      jambLocationName: "Back",
+        jambLocationValue: "back",
+      },
+    ]);
+
+    console.log("DoorJambLocation seed completed");
+  } else {
+    console.log("DoorJamblLocation already seeded");
+  }
+}
+
+
+
+
+async function seedDoorFrames() {
+  const exists = await DoorFrame.exists({});
+
+  if (!exists) {
+    await DoorFrame.insertMany([
+      {
+        frameName: "Full",
+        frameValue:"full",
+      },
+      {
+      frameName: "Half",
+       frameValue:"half",
+      },
+      {
+      frameName: "No",
+       frameValue:"no",
+      }
+    ]);
+
+    console.log("DoorFrame seed completed");
+  } else {
+    console.log("DoorFrame already seeded");
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
 async function seed() {
   try {
     await mongoose.connect(process.env.MONGO_URI);
 
     await seedDimensions();
     await seedDoorThresholds();
+    await seedDoorOrientations() 
+    await seedDoorjamblocations()
+    await seedDoorFrames()
 
     console.log("All seeds completed");
 

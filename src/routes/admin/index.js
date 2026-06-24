ad2b4b6f182e539a62ee81e01b35d4312f5eebc5
21ec043Thiyagu <146075318+21ec043Thiyagu@users.noreply.github.com>
2026-06-24 11:18:53 +0530

    modify seed_index.js

--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -6,7 +6,7 @@ router.use("/door-location",require("./doorLocationRoutes"));
 router.use("/door-thickness",require("./doorThicknessRoutes"));
 router.use("/frame-size",require("./frameSizeRoutes"));
 router.use("/dimension",require("./dimensionRoutes"));
-router.use("/design",require("./doorDesignRoutes"));
+router.use("/door-design",require("./doorDesignRoutes"));
 router.use("/door-orientation",require("./doorOrientationRoutes"));
 router.use("/door-jamb-location",require("./doorJambLocationRoutes"));
 router.use("/door-frame",require("./doorFrameRoutes"));

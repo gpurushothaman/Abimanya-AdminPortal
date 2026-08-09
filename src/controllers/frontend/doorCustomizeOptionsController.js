5ed7ae9a9d09f082f17703be0e0832a739f4416c
purushothaman <purushothaman@wealwin.com>
2026-08-09 17:15:20 +0530

    dropdown background option controller

--- src/controllers/frontend/doorCustomizeOptionsController.js
+++ src/controllers/frontend/doorCustomizeOptionsController.js
@@ -1,17 +1,23 @@
 const DoorLocation = require("../../models/admin/DoorLocation");
 const DoorDesign = require("../../models/admin/Design");
 const DoorSubDesign = require("../../models/admin/DoorSubDesign");
+const DoorModel = require("../../models/admin/DoorModel");
+const DoorShades = require("../../models/admin/DoorShades");
 
 exports.getAllDoorCustomizeOptions = async (req, res) => {
   try {
     const location = await DoorLocation.find();
     const design = await DoorDesign.find();
     const subDesign = await DoorSubDesign.find();
+    const doorModel = await DoorModel.find();
+    const doorShades = await DoorShades.find();
 
     const allOptions={
       location : location,
       design : design,
-      subDesign: subDesign
+      subDesign : subDesign,
+      models : doorModel, 
+      shades : doorShades
     };
 
     res.status(200).json({

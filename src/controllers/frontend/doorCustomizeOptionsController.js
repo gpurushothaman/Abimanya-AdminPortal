d7a55ee55cdd864217be9e6da4db0adb508ba0dd
purushothaman <purushothaman@wealwin.com>
2026-08-10 21:02:55 +0530

    push code

--- src/controllers/frontend/doorCustomizeOptionsController.js
+++ src/controllers/frontend/doorCustomizeOptionsController.js
@@ -3,6 +3,7 @@ const DoorDesign = require("../../models/admin/Design");
 const DoorSubDesign = require("../../models/admin/DoorSubDesign");
 const DoorModel = require("../../models/admin/DoorModel");
 const DoorShades = require("../../models/admin/DoorShades");
+const DoorFrame = require("../../models/admin/DoorFrame");
 
 exports.getAllDoorCustomizeOptions = async (req, res) => {
   try {
@@ -11,13 +12,15 @@ exports.getAllDoorCustomizeOptions = async (req, res) => {
     const subDesign = await DoorSubDesign.find();
     const doorModel = await DoorModel.find();
     const doorShades = await DoorShades.find();
+    const doorFrames = await DoorFrame.find();
 
     const allOptions={
       location : location,
       design : design,
       subDesign : subDesign,
       models : doorModel, 
-      shades : doorShades
+      shades : doorShades,
+      frames : doorFrames
     };
 
     res.status(200).json({
